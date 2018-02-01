f8c4321f07b8d4ec1490587fc718a35cae87ed71
Vaibhav Desai <vaidesai@paypal.com>
2018-02-01 15:55:25 -0800

    moved kickstartr dapp to a new repo

--- truffle/07-kickstarter-app/routes.js
+++
@@ -1,9 +0,0 @@
-const routes = require('next-routes')();
-
-routes
-    .add('/campaigns/new', '/campaigns/new')
-    .add('/campaigns/:address', '/campaigns/show')
-    .add('/campaigns/:address/spendingRequests', '/campaigns/spendingRequests/index')
-    .add('/campaigns/:address/spendingRequests/new', '/campaigns/spendingRequests/new');
-
-module.exports = routes;
